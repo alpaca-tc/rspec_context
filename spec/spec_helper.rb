8bfb475aa952843c920734997e3e06fa0e0f2177
alpaca-tc <alpaca-tc@alpaca.tc>
2017-11-10 06:04:47 +0900

    Setup rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,15 @@
 require "bundler/setup"
 require "rspec_context"
+require 'rspec_context'
+require 'pry' unless ENV['CI']
+
+Dir[File.join(File.expand_path('../support', __FILE__), '**', '*.rb')].each { |file| require(file) }
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  config.order = :random
 end
