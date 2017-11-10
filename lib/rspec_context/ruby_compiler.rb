91cea0f37ae324fa237a847395643893f383d138
alpaca-tc <alpaca-tc@alpaca.tc>
2017-11-10 18:55:31 +0900

    Create RSpecContext::RubyCompiler

---
+++ lib/rspec_context/ruby_compiler.rb
@@ -0,0 +1,47 @@
+require 'stringio'
+
+module RSpecContext
+  module RubyCompiler
+    class << self
+      def can_compile?(script)
+        suppress_output do
+          RubyVM::InstructionSequence.compile(script, nil, nil, nil, compile_options)
+        end
+
+        true
+      rescue SyntaxError
+        false
+      end
+
+      private
+
+      def suppress_output
+        original_stdout = $stdout.clone
+        original_stderr = $stderr.clone
+
+        null = File.open('/dev/null', 'w')
+        $stdout.reopen(null)
+        $stderr.reopen(null)
+
+        yield
+      ensure
+        $stdout.reopen(original_stdout)
+        $stderr.reopen(original_stderr)
+        null.close
+      end
+
+      def compile_options
+        {
+          inline_const_cache: false,
+          instructions_unification: false,
+          operands_unification: false,
+          peephole_optimization: false,
+          specialized_instruction: false,
+          stack_caching: false,
+          tailcall_optimization: false,
+          trace_instruction: false
+        }
+      end
+    end
+  end
+end
---
+++ spec/rspec_context/ruby_compiler_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe RSpecContext::RubyCompiler do
+  describe 'ClassMethods' do
+    describe '.can_compile?' do
+      subject { described_class.can_compile?(script) }
+
+      context 'with valid script' do
+        let(:script) { 'foo()' }
+        it { is_expected.to be true }
+      end
+
+      context 'with invalid script' do
+        let(:script) { 'foo do' }
+        it { is_expected.to be false }
+      end
+    end
+  end
+end
