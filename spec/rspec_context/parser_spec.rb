2b4527ad0d206d7757830f0e50821ed431adafa1
alpaca-tc <alpaca-tc@alpaca.tc>
2017-11-13 16:57:02 +0900

    Refactor parser_spec

--- spec/rspec_context/parser_spec.rb
+++ spec/rspec_context/parser_spec.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 RSpec.describe RSpecContext::Parser do
-  describe '#parse_spec_file' do
-    subject { instance.parse_spec_file }
-    let(:instance) { described_class.new(spec_file) }
-    let(:spec_file) { RSpecContext::SpecFile.new(file_path) }
-    let(:file_path) { fixtures_path.join('files/client.rb') }
+  describe 'InstanceMethods' do
+    describe '#parse_spec_file' do
+      subject { instance.parse_spec_file }
 
-    it { is_expected.to be_a(Array) }
-    it { is_expected.to all(be_a(RSpecContext::RSpecMethod)) }
+      let(:instance) { described_class.new(spec_file) }
+      let(:spec_file) { RSpecContext::SpecFile.new(file_path) }
+      let(:file_path) { fixtures_path.join('files/client.rb') }
+
+      it { is_expected.to be_a(Array) }
+      it { is_expected.to all(be_a(RSpecContext::RSpecMethod)) }
+    end
   end
 end
